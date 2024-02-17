b8cae2bcf10cf23267d9ee95cec2c30327f8e98c
brossa <surenlama751@gmail.com>
2024-02-17 12:20:22 +1100

    final code
    Social MERN STACK APP

---
+++ routes/authRoute.js
@@ -0,0 +1,38 @@
+import express from "express";
+import {registerController, loginController, testController, forgotPasswordController, updateProfileController} from '../controllers/authController.js';
+//router object
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
+
+
+
+const router = express.Router();
+
+//routing
+
+//REGISTER || METHOD POST
+router.post("/register", registerController);
+
+//login || POST
+router.post("/login", loginController);
+
+//Forgot Password || POST
+router.post("/forgot-password", forgotPasswordController);
+
+//test route
+router.get("/test", requireSignIn,isAdmin, testController);
+
+//protected user route auth
+router.get("/user-auth", requireSignIn, (req, res) => {
+  res.status(200).send({ ok: true });
+});
+
+//protected admin route auth
+router.get("/admin-auth", requireSignIn, isAdmin, (req, res) => {
+  res.status(200).send({ ok: true });
+});
+
+//update profile
+router.put("/profile", requireSignIn, updateProfileController);
+
+
+export default router;
